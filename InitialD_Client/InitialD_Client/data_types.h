38991d5c8e164167a37c1dbb23657ea47660b0b8
Douglas May <douglas.may@red-inc.us>
2018-02-28 17:06:42 -0500

    Packet Structure
    Header Data structure defined

---
+++ InitialD_Client/InitialD_Client/data_types.h
@@ -0,0 +1,13 @@
+#pragma once
+#define LOCATE_SERVER 1001
+
+
+typedef struct PacketHeader {
+	char protocol[9] = "InitialD";
+	char vers1[7] = "0.0.1a";
+	unsigned short action;
+	unsigned short data_len;
+};
+typedef struct LocateServer {
+	char name[32];
+}LocateServer;
